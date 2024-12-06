84cad19f4e95427a5221a4bf90889b2ccd977532
budiprasetio <budiprasetio1005@gmail.com>
2024-12-06 15:20:35 +0700

    Push

---
+++ js/script.js
@@ -0,0 +1,101 @@
+// const wordText = document.querySelector(".word"),
+//   hintText = document.querySelector(".hint span"),
+//   timeText = document.querySelector(".time b"),
+//   inputField = document.querySelector("input"),
+//   refreshBtn = document.querySelector(".refresh-word"),
+//   checkBtn = document.querySelector(".check-word");
+
+// let correctWord, timer;
+
+// const initTimer = (maxTime) => {
+//   clearInterval(timer);
+//   timer = setInterval(() => {
+//     if (maxTime > 0) {
+//       maxTime--;
+//       return (timeText.innerText = maxTime);
+//     }
+//     clearInterval(timer);
+//     alert(`Waktu habiss!!! ${userWord.toUpperCase()} jawaban kamu benar... :))`);
+//     initGame();
+//   }, 1000);
+// };
+
+// const initGame = () => {
+//   initTimer(30);
+//   let randomObj = words[Math.floor(Math.random() * words.length)];
+//   let wordArray = randomObj.word.split("");
+//   for (let i = wordArray.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1));
+//     [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+//   }
+//   wordText.innerText = wordArray.join(""); //meneruskan kata yang diacak sebagai teks kata
+//   hintText.innerText = randomObj.hint; //buat passing random object hint
+//   correctWord = randomObj.word.toLowerCase();
+//   inputField.value = "";
+//   inputField.setAttribute("maxlength", correctWord.length);
+//   console.log(randomObj);
+// };
+
+// initGame();
+
+// const checkWord = () => {
+//   let userWord = inputField.value.toLocaleLowerCase();
+//   if (!userWord) return alert("Please lah masukin kata katanya...");
+//   if (userWord !== correctWord) return alert(`Waduu ${userWord} masih salah nih... :(`);
+//   alert(`Mantapp ${userWord.toUpperCase()} jawaban kamu benar... :))`);
+// };
+
+// refreshBtn.addEventListener("click", initGame);
+// checkBtn.addEventListener("click", checkWord);
+
+const wordText = document.querySelector(".word"),
+  hintText = document.querySelector(".hint span"),
+  timeText = document.querySelector(".time b"),
+  inputField = document.querySelector("input"),
+  refreshBtn = document.querySelector(".refresh-word"),
+  checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = (maxTime) => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      return (timeText.innerText = maxTime);
+    }
+    clearInterval(timer);
+    alert(`Waktu habis! Jawaban yang benar adalah ${correctWord.toUpperCase()} :))`);
+    initGame();
+  }, 1000);
+};
+
+const initGame = () => {
+  initTimer(30);
+  let randomObj = words[Math.floor(Math.random() * words.length)];
+  let wordArray = randomObj.word.split("");
+  do {
+    for (let i = wordArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+  } while (wordArray.join("") === randomObj.word);
+  wordText.innerText = wordArray.join(""); // menampilkan kata yang diacak
+  hintText.innerText = randomObj.hint; // menampilkan hint
+  correctWord = randomObj.word.toLowerCase();
+  inputField.value = "";
+  inputField.setAttribute("maxlength", correctWord.length);
+};
+
+const checkWord = () => {
+  let userWord = inputField.value.toLowerCase();
+  if (!userWord) return alert("Harap masukkan kata terlebih dahulu...");
+  if (userWord !== correctWord) return alert(`Waduu ${userWord} masih salah nih... :(`);
+  alert(`Mantapp ${userWord.toUpperCase()} jawaban kamu benar... :))`);
+  initGame();
+};
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
+
+initGame();
---
+++ js/words.js
@@ -0,0 +1,118 @@
+const words = [
+  {
+    word: "TES",
+    hint: "Percobaan atau ujian",
+  },
+  {
+    word: "JAVA",
+    hint: "Bahasa pemrograman populer",
+  },
+  {
+    word: "PYTHON",
+    hint: "Bahasa pemrograman dan nama ular",
+  },
+  {
+    word: "NODE",
+    hint: "Runtime untuk JavaScript",
+  },
+  {
+    word: "WEB",
+    hint: "Halaman yang dapat diakses di internet",
+  },
+  {
+    word: "HTML",
+    hint: "Bahasa markup untuk membuat halaman web",
+  },
+  {
+    word: "CSS",
+    hint: "Digunakan untuk membuat gaya pada halaman web",
+  },
+  {
+    word: "FLASK",
+    hint: "Framework Python untuk pengembangan web",
+  },
+  {
+    word: "REACT",
+    hint: "Library JavaScript untuk membangun UI",
+  },
+  {
+    word: "API",
+    hint: "Antarmuka untuk berkomunikasi antara perangkat lunak",
+  },
+  {
+    word: "DEBUG",
+    hint: "Proses menemukan dan memperbaiki kesalahan dalam kode",
+  },
+  {
+    word: "COMPILE",
+    hint: "Proses menerjemahkan kode sumber ke kode mesin",
+  },
+  {
+    word: "SERVER",
+    hint: "Komputer yang menyediakan layanan untuk klien",
+  },
+  {
+    word: "BROWSER",
+    hint: "Alat untuk mengakses halaman web",
+  },
+  {
+    word: "GIT",
+    hint: "Sistem kontrol versi terdistribusi",
+  },
+  {
+    word: "BASH",
+    hint: "Shell populer di sistem operasi Linux",
+  },
+  {
+    word: "LINUX",
+    hint: "Sistem operasi berbasis kernel open source",
+  },
+  {
+    word: "MYSQL",
+    hint: "Sistem manajemen basis data relasional",
+  },
+  {
+    word: "TABLE",
+    hint: "Struktur data dalam basis data",
+  },
+  {
+    word: "ARRAY",
+    hint: "Struktur data untuk menyimpan elemen secara berurutan",
+  },
+  {
+    word: "STRING",
+    hint: "Tipe data yang terdiri dari karakter",
+  },
+  {
+    word: "BOOLEAN",
+    hint: "Tipe data dengan nilai benar atau salah",
+  },
+  {
+    word: "STACK",
+    hint: "Struktur data dengan prinsip LIFO",
+  },
+  {
+    word: "QUEUE",
+    hint: "Struktur data dengan prinsip FIFO",
+  },
+  {
+    word: "SOFTWARE",
+    hint: "Program yang berjalan di komputer",
+  },
+  {
+    word: "HARDWARE",
+    hint: "Komponen fisik dari komputer",
+  },
+  {
+    word: "ANDROID",
+    hint: "Sistem operasi untuk perangkat seluler",
+  },
+  {
+    word: "CLOUD",
+    hint: "Teknologi untuk penyimpanan dan pengolahan data online",
+  },
+  {
+    word: "ROUTER",
+    hint: "Alat untuk mengarahkan lalu lintas jaringan",
+  },
+];
